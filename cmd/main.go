bf54dd50717e1594456e7bd748de0a9c04cf6d19
Egor Khilik <khilik.egor@gmail.com>
2024-03-09 23:37:51 +0300

    connecting the database

--- cmd/main.go
+++ cmd/main.go
@@ -5,20 +5,41 @@ import (
 	"github.com/KhilikEgor/todo-app/pkg/hendler"
 	"github.com/KhilikEgor/todo-app/pkg/repository"
 	"github.com/KhilikEgor/todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("Error initializing cingings: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to initializing db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := hendler.NewHendler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("8080"), handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occurred while running http server: %s", err.Error())
 	}
 }
